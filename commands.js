a0b31b425c9c33ca652e6ef931654c08ad720d24
John Vincent <macutojohnvincent@gmail.com>
2024-09-02 20:45:08 +1200

    add, list, remove, update items. powershell buggy as of now.

---
+++ commands.js
@@ -0,0 +1,90 @@
+#!/usr/bin/env node
+
+// Import required modules
+const { Command } = require("commander");
+const inquirer = require("inquirer");
+const {
+  addItem,
+  findItem,
+  updateItem,
+  removeItem,
+  listItems,
+} = require("./index"); // Ensure this is correctly imported
+
+const program = new Command();
+const prompt = inquirer.createPromptModule();
+
+// Define questions for adding an item
+const addItemQuestions = [
+  {
+    type: "input",
+    name: "title",
+    message: "Enter item title",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter item description",
+  },
+  {
+    type: "input",
+    name: "starting_price",
+    message: "Enter starting price",
+  },
+  {
+    type: "input",
+    name: "reserve_price",
+    message: "Enter reserve price",
+  },
+];
+
+// Program version
+program.version("1.0.0").description("Auction CLI");
+
+// Add item command
+program
+  .command("add")
+  .alias("a")
+  .description("Add an item")
+  .action(() => {
+    prompt(addItemQuestions).then((answers) => addItem(answers));
+  });
+
+// Find item command
+program
+  .command("find <title>")
+  .alias("f")
+  .description("Find an item by title")
+  .action((title) => {
+    findItem(title);
+  });
+
+//update
+program
+  .command("update <_id>")
+  .alias("u")
+  .description("Update an item by id")
+  .action((_id) => {
+    prompt(addItemQuestions).then((answers) => updateItem(_id, answers));
+  });
+
+//remove item
+program
+  .command("remove <_id>")
+  .alias("r")
+  .description("Remove an item by id")
+  .action((_id) => {
+    removeItem(_id);
+  });
+
+//list items
+program
+  .command("list")
+  .alias("l")
+  .description("List all items")
+  .action(() => {
+    listItems();
+  });
+
+// Parse arguments
+program.parse(process.argv);
---
+++ index.js
@@ -0,0 +1,69 @@
+//mongoose database
+const mongoose = require("mongoose");
+
+//map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+
+//connect to database
+mongoose
+  .connect("mongodb://localhost:27017/auctiondatabase")
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+//import model
+const Item = require("./model/auction");
+
+// Add item
+const addItem = (item) => {
+  Item.create(item)
+    .then((item) => {
+      console.info("Item has been added", item);
+      mongoose.connection.close();
+    })
+    .catch((err) => console.log(err));
+};
+
+//find item
+const findItem = (title) => {
+  //make case insensitive
+  const search = new RegExp(title, "i");
+  Item.find({ $or: [{ title: search }] }).then((item) => {
+    console.info(item);
+    console.info(`${item.length} matches`);
+    mongoose.connection.close();
+  });
+};
+
+//update item
+const updateItem = (_id, item) => {
+  Item.findByIdAndUpdate({ _id }, item).then((item) => {
+    console.info("Item has been updated");
+    mongoose.connection.close();
+  });
+};
+
+//remove item
+const removeItem = (_id) => {
+  Item.deleteOne({ _id }).then((item) => {
+    console.info("Item has been removed");
+    mongoose.connection.close();
+  });
+};
+
+//list all items
+const listItems = () => {
+  Item.find().then((items) => {
+    console.info(items);
+    console.info(`${items.length} items`);
+    mongoose.connection.close();
+  });
+};
+
+// Export functions
+module.exports = {
+  addItem,
+  findItem,
+  updateItem,
+  removeItem,
+  listItems,
+};
