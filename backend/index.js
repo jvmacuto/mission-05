182dd81f8df9d252b98195f8002898be4c3889fe
CassJah <144091819+CassJah@users.noreply.github.com>
2024-10-08 12:42:05 +1300

    seeding

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const itemsRouter = require("./routes/routes"); // Ensure path to routes is correct
 
 const app = express();
-const port = process.env.PORT || 3005;
+const port = process.env.PORT || 3006;
 
 // Middleware to parse JSON requests
 app.use(express.json());
@@ -11,11 +11,14 @@ app.use(express.json());
 // Use the routes defined in routes.js with '/api' prefix
 app.use("/api", itemsRouter); // Prefix all routes with /api
 
-// Connect to MongoDB
-mongoose
-  .connect("mongodb://127.0.0.1:27017/auctiondatabase")
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+// Check if a mongoose connection already exists to avoid multiple connections
+if (mongoose.connection.readyState === 0) {
+  // Connect to MongoDB
+  mongoose
+    .connect("mongodb://127.0.0.1:27017/auctiondatabase")
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.error("MongoDB connection error:", err));
+}
 
 // Start the server
 app.listen(port, () => {
